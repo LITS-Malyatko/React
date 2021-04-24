dcb67d720b9fbc97f18bcb49db921d01e29a632b
Knyazik01 <ndrwkn2001@gmail.com>
2021-04-24 08:25:56 +0300

    fix for netlify

--- src/components/_parts/Footer/index.js
+++ src/components/_parts/Footer/index.js
@@ -10,9 +10,9 @@ function Footer() {
           <img className={style["page_footer_logo"]} src={logo} height="100%" alt='logo'/>
         </Link>
         <div className={style["page_footer_content_wrapper"]}>
-          <div className={style["page_footer_content_wrapper_terms"]}><a href="javascript:void(0)">умови користування сайтом</a></div>
-          <div className={style["page_footer_content_wrapper_rules"]}><a href="javascript:void(0)">правила публікацій та розсилок</a></div>
-          <div className={style["page_footer_content_wrapper_policy"]}><a href="javascript:void(0)">політика конфіденційності</a></div>
+          <div className={style["page_footer_content_wrapper_terms"]}><a href="/">умови користування сайтом</a></div>
+          <div className={style["page_footer_content_wrapper_rules"]}><a href="/">правила публікацій та розсилок</a></div>
+          <div className={style["page_footer_content_wrapper_policy"]}><a href="/">політика конфіденційності</a></div>
         </div>
       </div>
     </footer>
